b65e4dd8a7cbf870a0f92fc2ceb4b15973b945ce
Anthony Niemiec <aniemiec758@gmail.com>
2019-02-23 18:18:42 -0500

    mmap_speedtest write times

--- mmap_speedtest.c
+++ mmap_speedtest.c
@@ -1,19 +1,31 @@
 #include <iostream>
+#include <linux/memfd.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
 #include <chrono>
 #define PAGESIZE 4096 // 4KB in x86 systems
+#define MEMSIZE 1000
+
+void calcTime(auto start, auto finish) { // calculates time between two points to the nearest nanosecond
+	auto timeTaken = std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count();
+	std::cout << timeTaken << "ns, " << timeTaken/MEMSIZE << "ns per operation\n";
+}
 
 int main(int argc, char** argv) {
     // Part 0: setting up environment
-	int n = 1000;
-
 	// initializing malloc'd memory
-	char* mal = (char*)malloc(10 * sizeof(char));
+	char* mal = (char*)malloc(MEMSIZE * sizeof(char));
 
 	// initializing an anonymous file descriptor
-	int fSize = 50*PAGESIZE;
+	int fSize = MEMSIZE;
 	char fName[] = {"./anon_file"};
 	int inFile = memfd_create(fName, 0); // won't remain on disk
-	if inFile < 0) {
+	if (inFile < 0) {
 		perror("memfd_create");
 		exit(1);
 	}
@@ -30,8 +42,31 @@ int main(int argc, char** argv) {
 	}
 
     // Part 1: measuring write time of mmap vs malloc
+	// measuring malloc write time
 	auto start = std::chrono::high_resolution_clock::now();
-	
+	for (int i = 0; i < MEMSIZE; i++) {
+		memset(&mal[i], 'a', 1);
+	}
 	auto finish = std::chrono::high_resolution_clock::now();
-	auto timeTaken = std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count()
-	std::cout << "mmap: " << timeTaken << "ns, " << timeTaken/n << "ns per operation\n";
+	printf("Malloc: ");
+	calcTime(start, finish);
+
+	// measuring mmap write time
+	start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < MEMSIZE; i++) {
+		memset(&shared_mem[i], 'a', 1);
+	}
+	finish = std::chrono::high_resolution_clock::now();
+	printf("Mmap: ");
+	calcTime(start, finish);
+
+    // Part 2: measuring read time of mmap vs malloc
+	// measuring malloc read time
+	
+
+
+
+
+
+
+}
